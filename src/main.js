657ec80cf98e715b08bfc1180b85e214828e851d
zhangjiajia <756661181@qq.com>
2019-08-19 19:47:51 +0800

    初次提交

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'// 引入element-ui
+Vue.use(ElementUI)// 注册组件
 Vue.config.productionTip = false
 
 new Vue({
