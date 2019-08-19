490c70a74a85904e82b0375e00adca5789a4546b
zhangjiajia <756661181@qq.com>
2019-08-19 20:51:44 +0800

    初始化样式/清除多余的代代码

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入element-ui
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+import './style/index.css'// 引入初始化样式
 Vue.use(ElementUI)// 注册组件
 Vue.config.productionTip = false
 
