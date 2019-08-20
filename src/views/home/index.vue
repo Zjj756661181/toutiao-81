8e83e1e57ea796e7b9fc480b95209676b8ad24c4
zhangjiajia <756661181@qq.com>
2019-08-21 01:48:08 +0800

    右侧头部nav

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="home">
-    home
-  </div>
+  <!-- 最外边容器 -->
+  <el-container>
+    <!-- 左侧栏容器 -->
+    <el-aside class="el-aside">
+      <!-- 左侧菜单 -->
+      <layout-aside></layout-aside>
+    </el-aside>
+    <!-- 右侧嵌套大容器 -->
+    <el-container>
+      <!-- 头部内容 -->
+      <el-header>
+        <layout-header></layout-header>
+      </el-header>
+      <!-- 中间区域 -->
+      <el-main>zhong</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import layoutAside from '../../components/home/layout-aside'// 引入左侧菜单栏
+import layoutHeader from '../../components/home/layout-header'// 引入右侧头部nav
 export default {
-  name: 'home',
   components: {
-
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .el-aside {
+    width:200px;
+    height:100vh;
+    background-color:#323745
+  }
+
+</style>
