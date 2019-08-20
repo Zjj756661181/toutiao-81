4f92a3049788a568e3479b4aff535f50331cc72f
zhangjiajia <756661181@qq.com>
2019-08-20 21:30:20 +0800

    登录界面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="login">
     <el-card class="box-card">
-      <img src="../../assets/img/logo_index.png" alt="黑马头条" >
+      <div class="title">
+        <!-- title 图片 -->
+        <img src="../../assets/img/logo_index.png" alt="黑马头条" />
+      </div>
+      <!-- 表单结构 element  el-form  el-form-item -->
+      <el-form :model="formData" :rules="rules" ref="loginForm">
+        <!-- 输入框 因需校验用 el-form-item标签 包裹input -->
+        <!-- prop属性设置为需校验的字段 -->
+        <el-form-item prop="mobile">
+          <!-- 手机号绑定input -->
+          <el-input v-model="formData.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
 
-      <el-form :modle='formData' :rules='rules' ref="loginForm">
-          <el-form-item prop='mobile'>
-              <el-input v-model="formData.mobile" placeholder="请输入手机号"></el-input>
-          </el-form-item>
+        <el-form-item prop="code">
+          <!-- 验证码绑定input -->
+          <el-input v-model="formData.code" placeholder="请输入验证码" style="width:65%"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
 
-          <el-form-item prop='code'>
-              <el-input v-model="formData.code" placeholder="请输入验证码" style="width:65%"></el-input>
-              <el-button style="float:right">发送验证码</el-button>
-          </el-form-item>
+        <el-form-item prop="check">
+          <!-- checkbox绑定check -->
+          <el-checkbox v-model="formData.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
 
-          <el-form-item prop='check'>
-              <el-checkbox v-model="formData.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
-          </el-form-item>
-
-          <el-form-item>
-              <el-button @click="login" style="width:100%" type="primary">登录</el-button>
-          </el-form-item>
+        <el-form-item>
+          <!-- 设置点击事件login -->
+          <el-button @click="login" style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
       </el-form>
     </el-card>
   </div>
@@ -33,9 +42,9 @@ export default {
       // value 当前表单字段的值
       if (value) {
         // 满足校验
-        callback()// 同意继续往下走
+        callback() // 同意继续往下走
       } else {
-        // 不满足校验
+        // 不满足校验 error错误提示
         callback(new Error('用户协议和隐私条款未勾选，请认真阅读并确认！'))
       }
     }
@@ -61,17 +70,18 @@ export default {
         code: [
           {
             required: true,
-            massage: '验证码不能为空',
+            message: '验证码不能为空',
             trigger: 'blur'
           },
           {
             pattern: /^\d{6}$/,
-            massage: '验证码格式错误',
+            message: '验证码格式错误',
             trigger: 'blur'
           }
         ],
         ckeck: [
           {
+            // 自定义函数 (return上方）
             validator: func
           }
         ]
@@ -79,12 +89,34 @@ export default {
     }
   },
   methods: {
+    // 登录点击事件
     login () {
-      this.$refs.loginForm.validate((isOK, result) => {
+      // validate 手动校验表单
+      // isOK 是否通过校验
+      // result 当前校验结果
+      this.$refs.loginForm.validate(isOK => {
         if (isOK) {
-          this.$router.push('/home')
-        } else {
-          alert('')
+          this.$axios({
+            method: 'post',
+            url: '/authorizations',
+            data: this.formData
+          })
+            .then(result => {
+              // 缓存本地
+              window.localStorage.setItem(
+                'user-info',
+                JSON.stringify(result.data.data)
+              )
+              // 跳转页面 home
+              this.$router.push('/home')
+            })
+            .catch(() => {
+              // 警告弹框 登录不成功
+              this.$message({
+                message: '用户名或验证码错误',
+                type: 'warning'
+              })
+            })
         }
       })
     }
@@ -92,23 +124,26 @@ export default {
 }
 </script>
 
-<style lang='less'>
-    .login{
-        background-image: url('../../assets/img/login_bg.jpg');
-        width: 100%;
-        height: 100vh;
-        background-size: cover;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .box-card {
-            width: 400px;
-            height: 340px;
-            img{
-                display: block;
-                margin: 10px auto;
-                width: 200px;
-            }
-        }
+<style lang='less' scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
+    width: 400px;
+    height: 340px;
+    .title {
+      text-align: center;
+      img {
+        display: block;
+        margin: 10px auto;
+        width: 200px;
+      }
     }
+  }
+}
 </style>
